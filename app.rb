c45de5f436fbe26f46aa2051b8df22268413743a
Shintaro Inuzuka <graph226@gmail.com>
2016-04-08 23:59:43 +0900

    Add app.rb

---
+++ app.rb
@@ -0,0 +1,33 @@
+# Almostly taken from http://qiita.com/yuya_takeyama/items/0660a59d13e2cd0b2516#comment-e6737f3166be32597681
+
+require 'bundler/setup'
+require 'sinatra/base'
+require 'json'
+require 'rest-client'
+require 'sudden_death'
+
+post '/linebot/callback' do
+  params = JSON.parse(request.body.read)
+
+  params['result'].each do |msg|
+    request_content = {
+      to: [msg['content']['from']],
+      toChannel: 1383378250, # Fixed  value
+      eventType: "138311608800106203", # Fixed value
+      content: msg['content']
+    }
+
+    endpoint_uri = 'https://trialbot-api.line.me/v1/events'
+    content_json = request_content.to_json
+
+    RestClient.proxy = ENV["FIXIE_URL"]
+    RestClient.post(endpoint_uri, content_json, {
+      'Content-Type' => 'application/json; charset=UTF-8',
+      'X-Line-ChannelID' => ENV["LINE_CHANNEL_ID"],
+      'X-Line-ChannelSecret' => ENV["LINE_CHANNEL_SECRET"],
+      'X-Line-Trusted-User-With-ACL' => ENV["LINE_CHANNEL_MID"],
+    })
+  end
+
+  "OK"
+end
